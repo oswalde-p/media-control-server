dd67b1772af09b3c7e1ceeae52f0f9dd9dc52e5a
jason <j.s.pursey@gmail.com>
2019-03-04 22:27:04 +0100

    Add space and pause options

--- control-service.js
+++ control-service.js
@@ -3,13 +3,15 @@ const exec = require('child_process').exec
 // http://wiki.linuxquestions.org/wiki/XF86_keyboard_symbols
 const keyMap = {
   play: 'XF86AudioPlay',
+  pause: 'XF86AudioPause',
   volumeup: 'XF86AudioRaiseVolume',
   volumedown: 'XF86AudioLowerVolume',
   mute: 'XF86AudioMute',
   // skipfwd: 'SKIP_FWD',
   skipbwd: 'XF86AudioRewind',
   next: 'XF86AudioNext',
-  previous: 'XF86AudioPrev'
+  previous: 'XF86AudioPrev',
+  space: 'space'
 }
 
 function xDo(key){
