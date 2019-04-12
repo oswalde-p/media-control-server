f457a98a230fccc00b659f13c49ca4241eabd51c
jason <j.s.pursey@gmail.com>
2019-04-12 21:09:32 +0100

    remove localtunnel

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const controlService = require('./control-service')
-const localtunnel = require('localtunnel')
 
 const app = express()
 
 const PORT = process.env.PORT || 8675
-const SUBDOMAIN = process.env.SUBDOMAIN || 'shabado620062'
 
 app.get('/', (req, res) => res.send(200))
+
+app.get('/ping', (req, res) => res.sendStatus(200))
 
 app.get('/:command', (req, res) => {
   const { command } = req.params
   try {
     controlService.handle(command)
-    return res.status(200).send(command)
+    return res.status(200).send({command})
   } catch(err) {
     if (err.message.split(':' === 'ARGUMENT ERROR')) {
       return res.sendStatus(400)
@@ -25,17 +25,5 @@ app.get('/:command', (req, res) => {
 
 app.listen(PORT, () => console.log('Listening on port ' + PORT))
 
-const tunnel = localtunnel(PORT, { subdomain: SUBDOMAIN }, function(err, tunnel) {
-  if (err) console.log(err)
-  console.log('Public url: ' + tunnel.url)
-})
 
-tunnel.on('error', (err) => {
-  console.log(err)
-})
 
-tunnel.on('close', function() {
-  // tunnels are closed
-});
-
-;
