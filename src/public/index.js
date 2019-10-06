dc2bd981edd52336fdeacde143a15ea400b4796c
Jason Pursey <jason@unumotors.com>
2019-10-07 00:41:43 +0200

    Add more buttons

--- src/public/index.js
+++ src/public/index.js
@@ -16,12 +16,8 @@ if ('mediaSession' in navigator) {
     ]
   });
 
-  navigator.mediaSession.setActionHandler('play', play);
-  navigator.mediaSession.setActionHandler('pause', pause);
-  navigator.mediaSession.setActionHandler('seekbackward', function() { /* Code excerpted. */ });
-  navigator.mediaSession.setActionHandler('seekforward', function() { /* Code excerpted. */ });
-  navigator.mediaSession.setActionHandler('previoustrack', function() { /* Code excerpted. */ });
-  navigator.mediaSession.setActionHandler('nexttrack', function() { /* Code excerpted. */ });
+  navigator.mediaSession.setActionHandler('play', play)
+  navigator.mediaSession.setActionHandler('pause', pause)
 }
 
 let isAudioPlaying = false
@@ -36,18 +32,44 @@ function sendToServer(command){
 const playButton = document.getElementById('button-play')
 playButton.addEventListener('click', () => isAudioPlaying ? pause() : play())
 
+const stopButton = document.getElementById('button-stop')
+stopButton.addEventListener('click', stop)
+
+const upButton = document.getElementById('button-up')
+upButton.addEventListener('click', () => sendToServer('up'))
+const downButton = document.getElementById('button-down')
+downButton.addEventListener('click', () => sendToServer('down'))
+const leftButton = document.getElementById('button-left')
+leftButton.addEventListener('click', () => sendToServer('left'))
+const rightButton = document.getElementById('button-right')
+rightButton.addEventListener('click', () => sendToServer('right'))
+
+const selectButton = document.getElementById('button-select')
+selectButton.addEventListener('click', () => sendToServer('return'))
+
+const volumeUpButton = document.getElementById('button-volume-up')
+volumeUpButton.addEventListener('click', () => sendToServer('volumeup'))
+const volumeDownButton = document.getElementById('button-volume-down')
+volumeDownButton.addEventListener('click', () => sendToServer('volumedown'))
 
 function play() {
   document.querySelector('audio').play()
-  sendToServer('play')
+  sendToServer('space')
   isAudioPlaying = true
-  playButton.value = 'Pause'
+  playButton.innerHTML = 'Pause'
 }
 
 function pause() {
   document.querySelector('audio').pause()
-  sendToServer('pause')
+  sendToServer('space')
   isAudioPlaying = false
-  playButton.value = 'Play'
+  playButton.innerHTML = 'Play'
+}
+
+function stop() {
+  document.querySelector('audio').pause()
+  sendToServer('backspace')
+  isAudioPlaying = false
+  playButton.innerHTML = 'Play'
 }
 
