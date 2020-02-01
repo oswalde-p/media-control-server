30f4a363723df9c48432bb52810e628f65f06dd8
oswalde-p <31041273+oswalde-p@users.noreply.github.com>
2020-02-01 14:55:02 +0100

    Add web ui (#1)
    * Move things to src/
    
    * Serve static page with basic controls on /
    
    * Add more buttons
    
    * Style buttons a bit
    
    * Remove outline for buttons
    
    * Change button style
    
    * Adjust stop button, add theme-color
    
    * Start adding vol slider
    
    * Add button to change play state

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 const express = require('express')
 const ip = require('ip')
+const path = require('path')
 const controlService = require('./control-service')
+const volumeService = require('./volume')
 
 const app = express()
 
 const PORT = process.env.PORT || 8675
 
-app.get('/', (req, res) => res.sendStatus(200))
+app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/', (req, res) => res.render('./public/index.html'))
+app.get('/favicon.ico', (req, res) => res.render('./public/favicon.ico'))
 app.get('/ping', (req, res) => res.sendStatus(200))
+
+app.get('/volume/:level', (req, res) => {
+  const { level } = req.params
+  try{
+    volumeService.setVolume(level)
+    res.status(200).send(level)
+  } catch(err){
+    console.error(err)
+    res.status(500).send()
+  }
+})
+
+app.get('/volume', (req, res) => {
+  try{
+    const level = volumeService.getVolume()
+    res.status(200).send({level})
+  } catch(err){
+    console.error(err)
+    res.status(500).send()
+  }
+})
 
 app.get('/:command', (req, res) => {
   const { command } = req.params
---
+++ src/public/index.js
@@ -0,0 +1,120 @@
+const SERVER_URL = window.location.href
+const currentVolume = 10
+// Add notification info if it's supported
+if ('mediaSession' in navigator) {
+  navigator.mediaSession.metadata = new MediaMetadata({
+    title: 'R*moat',
+    artist: 'Connected',
+    album: '',
+    artwork: [
+      { src: 'https://dummyimage.com/96x96',   sizes: '96x96',   type: 'image/png' },
+      { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
+      { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
+      { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
+      { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
+      { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
+    ]
+  });
+
+  navigator.mediaSession.setActionHandler('play', play)
+  navigator.mediaSession.setActionHandler('pause', pause)
+}
+
+let isAudioPlaying = false
+
+function sendToServer(command){
+  const req = new XMLHttpRequest()
+  req.open('GET', `${SERVER_URL}${command}`)
+  req.send()
+}
+
+// add event listeners for dom elements
+const playButton = document.getElementById('button-play')
+playButton.addEventListener('click', () => isAudioPlaying ? pause() : play())
+
+const stopButton = document.getElementById('button-stop')
+stopButton.addEventListener('click', stop)
+
+const fixStateButton = document.getElementById('button-fix-state')
+fixStateButton.addEventListener('click', swapPlayState)
+
+const upButton = document.getElementById('button-up')
+upButton.addEventListener('click', () => sendToServer('up'))
+const downButton = document.getElementById('button-down')
+downButton.addEventListener('click', () => sendToServer('down'))
+const leftButton = document.getElementById('button-left')
+leftButton.addEventListener('click', () => sendToServer('left'))
+const rightButton = document.getElementById('button-right')
+rightButton.addEventListener('click', () => sendToServer('right'))
+
+const selectButton = document.getElementById('button-select')
+selectButton.addEventListener('click', () => sendToServer('return'))
+
+const volumeUpButton = document.getElementById('button-volume-up')
+volumeUpButton.addEventListener('click', () => {
+  sendToServer('volumeup')
+  getVolume()
+})
+const volumeDownButton = document.getElementById('button-volume-down')
+volumeDownButton.addEventListener('click', () => {
+  sendToServer('volumedown')
+  getVolume()
+})
+
+function play() {
+  document.querySelector('audio').play()
+  sendToServer('space')
+  isAudioPlaying = true
+  playButton.innerHTML = 'Pause'
+  playButton.classList.remove('paused')
+}
+
+function pause() {
+  document.querySelector('audio').pause()
+  sendToServer('space')
+  isAudioPlaying = false
+  playButton.innerHTML = 'Play'
+  playButton.classList.add('paused')
+}
+
+function stop() {
+  document.querySelector('audio').pause()
+  sendToServer('backspace')
+  isAudioPlaying = false
+  playButton.innerHTML = 'Play'
+}
+
+function swapPlayState() {
+  isAudioPlaying = !isAudioPlaying
+  if (isAudioPlaying) {
+    playButton.innerHTML = 'Pause'
+    playButton.classList.remove('paused')
+    return
+  }
+  playButton.innerHTML = 'Play'
+  playButton.classList.add('paused')
+}
+
+// initialise the volume level
+getVolume()
+
+function updateVolume(vol){
+  setVolume(vol)
+}
+
+function getVolume() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", function() {
+    const level = JSON.parse(this.responseText).level
+    document.getElementById("range-volume").value = level
+  })
+  req.open('GET', `${SERVER_URL}volume`)
+  req.send()
+}
+
+function setVolume(level) {
+  const req = new XMLHttpRequest()
+  req.open('GET', `${SERVER_URL}volume/${level}`)
+  req.send()
+}
+
